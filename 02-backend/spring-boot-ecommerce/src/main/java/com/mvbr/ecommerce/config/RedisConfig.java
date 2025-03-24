422c9ff3caf162f4696cde5729714e92484ff033
Marcus Rangel <marcus.vbrangel@gmail.com>
2025-03-24 00:41:29 -0300

    feat: implemented a spring cache with redis but wont work

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/mvbr/ecommerce/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.mvbr.ecommerce.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.json()));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfig)
+                .build();
+    }
+
+}
--- 02-backend/spring-boot-ecommerce/src/main/java/com/mvbr/ecommerce/dao/CountryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/mvbr/ecommerce/dao/CountryRepository.java
@@ -2,15 +2,42 @@ package com.mvbr.ecommerce.dao;
 
 import com.mvbr.ecommerce.entity.Country;
 import com.mvbr.ecommerce.projection.CountryProjection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.util.List;
+
+
 // http://localhost:8085/api/countries
 // http://localhost:8085/api/countries/1
+
+// http://localhost:8085/api/countries/search/findAllCached
 
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "countries",
         path = "countries", excerptProjection = CountryProjection.class)
 public interface CountryRepository extends JpaRepository<Country, Long> {
+
+    Logger logger = LoggerFactory.getLogger(CountryRepository.class);
+
+    @Cacheable("countries")
+    default List<Country> findAllCached() {
+        logger.info("Fetching countries from database");
+        return findAll();
+    }
+
+//    @Cacheable("countries")
+//    List<Country> findAll();
+
+    @Override
+    @Cacheable("countries")
+    default List<Country> findAll() {
+        logger.info("Fetching countries from database");
+        return findAll();
+    }
+
 }
