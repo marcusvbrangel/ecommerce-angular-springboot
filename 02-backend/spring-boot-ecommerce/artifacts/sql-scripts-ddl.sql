663967862ed529bc787235674107aff7f3186605
Marcus Rangel <marcus.vbrangel@gmail.com>
2025-04-07 21:39:35 -0300

    feat: implemented the entities and its relationsships

--- 02-backend/spring-boot-ecommerce/artifacts/sql-scripts-ddl.sql
+++ 02-backend/spring-boot-ecommerce/artifacts/sql-scripts-ddl.sql
@@ -34,6 +34,64 @@ CREATE TABLE IF NOT EXISTS state (
 );
 
 
+----------------------------------------------------------
+
+
+CREATE TABLE IF NOT EXISTS address (
+ id BIGSERIAL PRIMARY KEY,
+  city VARCHAR(255) DEFAULT NULL,
+  country VARCHAR(255) DEFAULT NULL,
+  state VARCHAR(255) DEFAULT NULL,
+  street VARCHAR(255) DEFAULT NULL,
+  zip_code VARCHAR(255) DEFAULT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS customer (
+  id BIGSERIAL PRIMARY KEY,
+  first_name VARCHAR(255) DEFAULT NULL,
+  last_name VARCHAR(255) DEFAULT NULL,
+  email VARCHAR(255) DEFAULT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS orders (
+    id BIGSERIAL PRIMARY KEY,
+    order_tracking_number VARCHAR(255) DEFAULT NULL,
+    total_price DECIMAL(19,2) DEFAULT NULL,
+    total_quantity INT DEFAULT NULL,
+    billing_address_id BIGINT DEFAULT NULL,
+    customer_id BIGINT DEFAULT NULL,
+    shipping_address_id BIGINT DEFAULT NULL,
+    status VARCHAR(128) DEFAULT NULL,
+    date_created TIMESTAMP(6) DEFAULT NULL,
+    last_updated TIMESTAMP(6) DEFAULT NULL,
+    UNIQUE (billing_address_id),
+    UNIQUE (shipping_address_id),
+    FOREIGN KEY (customer_id) REFERENCES customer (id),
+    FOREIGN KEY (billing_address_id) REFERENCES address (id),
+    FOREIGN KEY (shipping_address_id) REFERENCES address (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS order_item (
+    id BIGSERIAL PRIMARY KEY,
+    image_url VARCHAR(255) DEFAULT NULL,
+    quantity INT DEFAULT NULL,
+    unit_price DECIMAL(19,2) DEFAULT NULL,
+    order_id BIGINT DEFAULT NULL,
+    product_id BIGINT DEFAULT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders (id),
+    FOREIGN KEY (product_id) REFERENCES product (id)
+);
+
+
+
+
+
+
+
+
 
 
 
